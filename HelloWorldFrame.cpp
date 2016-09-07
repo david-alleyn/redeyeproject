b6f8d02cb4ebd6a89563cc73b6beba58ec5c359c
David Alleyn <david.alleyn@gmail.com>
2016-09-07 14:45:28 -0400

    I have uploaded a working VS project with wxWidgets included. There is a small HelloWorld Demo class inside of HelloWorldFrame

---
+++ HelloWorldFrame.cpp
@@ -0,0 +1,45 @@
+// For compilers that support precompilation, includes "wx/wx.h".
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+#include "HelloWorldFrame.h"
+
+//EVT_MENU(ID_Hello, MyFrame::OnHello)
+//EVT_MENU(wxID_EXIT, MyFrame::OnExit)
+//EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
+
+MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
+	: wxFrame(NULL, wxID_ANY, title, pos, size)
+{
+	Bind(wxEVT_COMMAND_MENU_SELECTED, &MyFrame::OnHello, this, ID_Hello);
+	Bind(wxEVT_COMMAND_MENU_SELECTED, &MyFrame::OnExit, this, wxID_EXIT);
+	Bind(wxEVT_COMMAND_MENU_SELECTED, &MyFrame::OnAbout, this, wxID_ABOUT);
+
+	wxMenu *menuFile = new wxMenu;
+	menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",	"Help string shown in status bar for this menu item");
+	menuFile->AppendSeparator();
+	menuFile->Append(wxID_EXIT);
+	wxMenu *menuHelp = new wxMenu;
+	menuHelp->Append(wxID_ABOUT);
+	wxMenuBar *menuBar = new wxMenuBar;
+	menuBar->Append(menuFile, "&File");
+	menuBar->Append(menuHelp, "&Help");
+	SetMenuBar(menuBar);
+	CreateStatusBar();
+	SetStatusText("Welcome to wxWidgets!");
+}
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+	Close(true);
+}
+void MyFrame::OnAbout(wxCommandEvent& event)
+{
+	wxMessageBox("This is a wxWidgets' Hello world sample",
+		"About Hello World", wxOK | wxICON_INFORMATION);
+}
+void MyFrame::OnHello(wxCommandEvent& event)
+{
+	wxLogMessage("Hello world from wxWidgets!");
+}
---
+++ HelloWorldFrame.h
@@ -0,0 +1,22 @@
+#pragma once
+// For compilers that support precompilation, includes "wx/wx.h".
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+class MyFrame : public wxFrame
+{
+public:
+	MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+private:
+	void OnHello(wxCommandEvent& event);
+	void OnExit(wxCommandEvent& event);
+	void OnAbout(wxCommandEvent& event);
+};
+
+
+
+enum
+{
+	ID_Hello = 1
+};
